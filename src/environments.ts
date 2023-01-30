b8cdc1798afb9886a71992a795f8b260d2cf9fae
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-01-30 23:19:05 +0300

    url

--- src/environments.ts
+++ src/environments.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  // apiUrl: 'https://mean-deploy-api-production.up.railway.app/api'
-  apiUrl: 'http://localhost:3000/api'
+  apiUrl: 'https://mean-deploy-api-production.up.railway.app/api'
+  // apiUrl: 'http://localhost:3000/api'
 };
