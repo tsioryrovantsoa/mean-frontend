ce2b5bd21078cc1d4d22984d8f9b14b2a39b4c19
Tsiory Rakotoarimalala <80772886+tsiory-rovantsoa@users.noreply.github.com>
2023-01-30 21:41:35 +0300

    ajustement affichage et formatage de donnee

--- src/app/voitures/voiture-create/car-create.component.ts
+++ src/app/voitures/voiture-create/car-create.component.ts
@@ -8,11 +8,16 @@ import { VoitureService } from '../voitures.service';
   templateUrl: './car-create.component.html',
   styleUrls: ['./car-create.component.css'],
 })
-export class CreationVoiture {
+export class CreationVoiture implements OnInit, OnDestroy{
   isLoading = false;
   isSuccess = false;
   form: FormGroup;
   constructor(public voitureService: VoitureService, private router: Router) {}
+
+  ngOnInit(): void {
+// this.isLoading=true;
+  }
+
   Creervoiture(form: NgForm) {
     if (form.invalid) {
       return;
@@ -20,5 +25,9 @@ export class CreationVoiture {
     console.log(form.value);
     this.voitureService.createVoiture(form.value.matricule);
     form.reset();
+  }
+
+  ngOnDestroy() {
+    // this.isLoading=false;
   }
 }
--- src/environments.ts
+++ src/environments.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  apiUrl: 'https://mean-deploy-api-production.up.railway.app/api'
+  // apiUrl: 'https://mean-deploy-api-production.up.railway.app/api'
+  apiUrl: 'http://localhost:3000/api'
 };
